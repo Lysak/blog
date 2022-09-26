8ede322a8e42beedf247e33d26a6cc24eeac430d
Dmytrii Lysak <dlysak@live.com>
2022-09-26 13:24:03 +0300

    clean code

--- utils/global-data.js
+++ utils/global-data.js
@@ -5,9 +5,11 @@ export const getGlobalData = () => {
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+  // const footerText = process.env.BLOG_FOOTER_TEXT
+  //   ? decodeURI(process.env.BLOG_FOOTER_TEXT)
+  //   : 'All rights reserved.';
+
+  const footerText = 'Copyright © 2022 Dmytrii Lysak. All rights reserved.';
 
   return {
     name,
